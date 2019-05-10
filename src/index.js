daf1e7227e7dd87156855031b746b941fb9290bd
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-05-10 12:01:41 -0500

    npm and modules exercise

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+
+const sayHello = (greeting) => {
+    console.log("hello")
+};
+sayHello();
+
+const data = require('./say-hello.js');
---
+++ src/say-hello.js
@@ -0,0 +1,11 @@
+
+
+const salutation = () => {
+    console.log("Good Afternoon!");
+};
+salutation();
+
+const bye =() => {
+    console.log("See ya later!");
+};
+bye();
